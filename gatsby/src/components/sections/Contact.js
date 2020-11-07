dd2d962968ce43cff15fc382a3c6da71a61790a5
Dustin Simensen <dustinsimensen@gmail.com>
2020-11-06 20:30:16 -0800

    changed placeholders

--- gatsby/src/components/sections/Contact.js
+++ gatsby/src/components/sections/Contact.js
@@ -46,66 +46,70 @@ const Form = styled.form`
   }
 `;
 
-const Contact = () => {
-  return (
-    <section id="contact">
-      <Form
-        method="post"
-        netlify-honeypot="bot-field"
-        data-netlify="true"
-        name="contact"
-      >
-        <div className="form-header">
-          <h1 className="header">Contact Me</h1>
-          <p>
-            Want to get in <Reverse>touch?</Reverse>
-          </p>
-        </div>
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Jane Doe"
-            required
-          />
-        </label>
-        <label>
-          Email
-          <input
-            type="email"
-            name="email"
-            id="email"
-            required
-            placeholder="JaneDoe@who.com"
-          />
-        </label>
-        <label>
-          Subject
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            required
-            placeholder="Subject Title"
-          />
-        </label>
-        <label>
-          Message
-          <textarea name="message" id="message" rows="5" required />
-        </label>
-        <div className="buttons">
-          <Button type="submit">Send</Button>
-          <Button type="reset" value="Clear">
-            Clear
-          </Button>
-        </div>
-      </Form>
-    </section>
-  );
-};
+const Contact = () => (
+  <section id="contact">
+    <Form
+      method="post"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      name="contact"
+    >
+      <div className="form-header">
+        <h1 className="header">Contact Me</h1>
+        <p>
+          Want to get in <Reverse>touch?</Reverse>
+        </p>
+      </div>
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Jane Doe"
+          required
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          placeholder="JaneDoe@who.com"
+        />
+      </label>
+      <label>
+        Subject
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          required
+          placeholder="Hello Dustin"
+        />
+      </label>
+      <label>
+        Message
+        <textarea
+          name="message"
+          id="message"
+          rows="5"
+          required
+          placeholder="Hey Dustin, I want to hire you!"
+        />
+      </label>
+      <div className="buttons">
+        <Button type="submit">Send</Button>
+        <Button type="reset" value="Clear">
+          Clear
+        </Button>
+      </div>
+    </Form>
+  </section>
+);
 
 export default Contact;
